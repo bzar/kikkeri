7dfacbeb2425c74cf1023e90d36da0862d052146
Teemu Erkkola <teemu.erkkola@iki.fi>
2014-10-18 16:39:48 +0300

    Added SCSS styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var gulpLiveScript = require('gulp-livescript');
 var nodemon = require('gulp-nodemon');
+var sass = require('gulp-sass');
 
 gulp.task('default', ['build']);
 
-gulp.task('build', ['ls-server', 'ls-client', 'views', 'web']);
+gulp.task('build', ['ls-server', 'ls-client', 'views', 'web', 'sass']);
 
 gulp.task('ls-server', function() {
   return gulp.src('./src/*.ls')
@@ -21,6 +22,11 @@ gulp.task('web', function() {
   return gulp.src('./web/*')
     .pipe(gulp.dest('build/web'));
 });
+gulp.task('sass', function() {
+  return gulp.src('./sass/*')
+    .pipe(sass())
+    .pipe(gulp.dest('build/web'));
+});
 gulp.task('views', function() {
   return gulp.src('./views/*.jade')
     .pipe(gulp.dest('build/views'));
@@ -31,6 +37,7 @@ gulp.task('watch', function() {
   gulp.watch('src/*.ls', ['ls-server']);
   gulp.watch('websrc/*.ls', ['ls-client']);
   gulp.watch('web/*', ['web']);
+  gulp.watch('sass/*', ['sass']);
   gulp.watch('views/*.jade', ['views']);
 });
 
